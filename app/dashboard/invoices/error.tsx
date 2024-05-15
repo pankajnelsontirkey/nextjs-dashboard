35166afdcd259102e200cbd3c454202d66b09268
Pankaj Nelson Tirkey <pankajnelsontirkey@users.noreply.github.com>
2024-05-15 15:58:04 +0530

    Update error.tsx added type for error and reset

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -2,7 +2,12 @@
 
 import { useEffect } from 'react';
 
-export default function Error({ error, reset }) {
+export default function Error({ error, reset
+}: {
+  error: Error & { digest?: string },
+  reset: () => void
+})
+  {
   useEffect(() => {
     console.error(error);
   }, [error]);
